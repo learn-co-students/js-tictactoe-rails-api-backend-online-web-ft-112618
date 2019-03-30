bfd7d9e94eec34e76e2112d17ce213b93efa4e4c
Elise Phan <aliceconnic@gmail.com>
2019-03-30 00:15:10 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,23 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    game = Game.create(game_params)
-    render json: game, status: 201
+    @game = Game.create(game_params)
+    render json: @game, status: 201
   end
   
   def show 
     @game = Game.find(params[:id])
+    render json: @game
+  end
+  
+  def index
+    @games = Game.all
+    render json: @games
+  end
+  
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
     render json: @game
   end
   
